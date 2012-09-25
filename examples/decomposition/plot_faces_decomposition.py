bcba3006c94d81f5343329672ef4a2c731386eed
Olivier Mangin <olivier.mangin@inria.fr>
2012-09-25 15:21:04 +0200

    Minor mods to KLdivNMF.
    - Fixes NMF random test that was not reliable enough.
    - Adds flag to force parameter scaling in update when not in fit mode.
    - Cosmetics.

--- examples/decomposition/plot_faces_decomposition.py
+++ examples/decomposition/plot_faces_decomposition.py
@@ -76,7 +76,8 @@ estimators = [
      False),
 
     ('Non-negative components - KL NMF',
-     decomposition.KLdivNMF(n_components=n_components, init='nndsvdar', tol=1e-4),
+     decomposition.KLdivNMF(n_components=n_components, init='nndsvdar',
+         tol=1e-4),
      False),
 
     ('Independent components - FastICA',
@@ -134,5 +135,5 @@ for name, estimator, center in estimators:
                      n_row=1)
     plot_gallery('%s - Train time %.1fs' % (name, train_time),
                  components_[:n_components])
-    
+
 pl.show()
